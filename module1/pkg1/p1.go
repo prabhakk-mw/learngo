403ffb6bcbbafaabc2b14ee0ea907da9a921a95f
Prabhakar Kumar <prabhakk@mathworks.com>
2025-05-26 16:57:56 +0530

    Updating print

--- module1/pkg1/p1.go
+++ module1/pkg1/p1.go
@@ -1,5 +1,5 @@
 package pkg1
 
 func Test() string {
-	return "in Pkg1/P1.go"
+	return "from GITHUB in Pkg1/P1.go"
 }
--- module1/pkg2/p2.go
+++ module1/pkg2/p2.go
@@ -1,5 +1,5 @@
 package pkg2
 
 func Test() string {
-	return "in Pkg2/P2.go"
+	return "From GITHUB Pkg2/P2.go"
 }
